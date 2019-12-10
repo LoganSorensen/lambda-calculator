314a7518d7b39e56ef9b049526224fab3d4a9b7f
Logan Sorensen <loganmsorensen@gmail.com>
2019-12-10 14:14:24 -0800

    added buttons to the DOM

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,7 +3,11 @@ import React from "react";
 const OperatorButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      <button></button>
+      }
     </>
   );
 };
+
+export default OperatorButton;
